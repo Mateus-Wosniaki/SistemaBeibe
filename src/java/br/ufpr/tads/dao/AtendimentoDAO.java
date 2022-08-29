069197764b6f5d59c38b152d04326b1b712e6968
Gabril <gjperes@ufpr.br>
2022-08-29 12:28:21 -0300

    feature:wip - DAOs + revertendo o commit b*sta

--- src/java/br/ufpr/tads/dao/AtendimentoDAO.java
+++ src/java/br/ufpr/tads/dao/AtendimentoDAO.java
@@ -4,10 +4,70 @@
  */
 package br.ufpr.tads.dao;
 
+import java.sql.Connection;
+import br.ufpr.tads.beans.Atendimento;
+import br.ufpr.tads.exception.DAOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Gabril
  */
-public class AtendimentoDAO {
-    
+public class AtendimentoDAO implements DAO<Atendimento> {
+    private static final String INSERIR = "insert into MUDAR (*, *, *) values (?, ?, ?)";
+    private static final String BUSCARTODOS = "select * from MUDAR";
+    private static final String BUSCARPORID = "select * from MUDAR where MUDAR = ?";
+    private static final String DELETAR = "delete from MUDAR where MUDAR = ?";
+    private static final String ATUALIZAR = "update MUDAR set MUDAR = ?, MUDAR = ?, MUDAR = ? where MUDAR = ?";
+
+    private Connection con = null;
+
+    @Override
+    public Atendimento buscar(int id) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(BUSCARPORID)) {
+            // TODO: implementar
+            return null;
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao recuperar o atendimento ID: " + id, e);
+        }
+    }
+
+    @Override
+    public List<Atendimento> buscarTodos() throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(BUSCARTODOS)) {
+            // TODO: implementar
+            return null;
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao recuperar todos atendimentos", e);
+        }
+    }
+
+    @Override
+    public void inserir(Atendimento atendimento) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(INSERIR)) {
+            // TODO: implementar
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao inserir o atendimento: " + atendimento.getAtendimentoId(), e);
+        }
+    }
+
+    @Override
+    public void atualizar(Atendimento atendimento) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(ATUALIZAR)) {
+            // TODO: implementar
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao atualizar o atendimento: " + atendimento.getAtendimentoId(), e);
+        }
+    }
+
+    @Override
+    public void remover(Atendimento atendimento) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(DELETAR)) {
+            // TODO: implementar
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao remover o atendimento: " + atendimento.getAtendimentoId(), e);
+        }
+    }
 }
--- src/java/br/ufpr/tads/dao/UsuarioDAO.java
+++ src/java/br/ufpr/tads/dao/UsuarioDAO.java
@@ -9,6 +9,9 @@ import br.ufpr.tads.exception.DAOException;
 import java.sql.Connection;
 import java.util.List;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -33,27 +36,102 @@ public class UsuarioDAO implements DAO<Usuario> {
 
     @Override
     public Usuario buscar(int id) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try (PreparedStatement st = con.prepareStatement(BUSCARPORID)) {
+            Usuario usuario = new Usuario();
+            
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            
+            if (rs.next()) {
+                usuario.setUsuarioId(id);
+                usuario.setNomeCompleto(rs.getString("nomeCompleto"));
+                usuario.setCpf(rs.getString("cpf"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setTelefone(rs.getString("telefone"));
+                // TODO: usuario.setFuncao e usuario.setEndereco
+            }
+            
+            return usuario;
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao recuperar o usuário ID: " + id, e);
+        }
     }
 
     @Override
     public List<Usuario> buscarTodos() throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try (PreparedStatement st = con.prepareStatement(BUSCARTODOS)) {
+            List<Usuario> usuarios = new ArrayList<>();
+            
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                
+                usuario.setUsuarioId(rs.getInt("id"));
+                usuario.setNomeCompleto(rs.getString("nomeCompleto"));
+                usuario.setCpf(rs.getString("cpf"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setTelefone(rs.getString("telefone"));
+                // TODO: usuario.setFuncao e usuario.setEndereco
+                
+                usuarios.add(usuario);
+            }
+            
+            return usuarios;
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao recuperar todos usuários", e);
+        }
     }
 
     @Override
-    public void inserir(Usuario t) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void inserir(Usuario usuario) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(INSERIR)) {
+            st.setString(1, usuario.getNomeCompleto());
+            st.setString(2, usuario.getEmail());
+            st.setString(3, usuario.getCpf());
+            st.setString(4, usuario.getTelefone());
+            st.setString(5, usuario.getSenha());
+            st.setInt(6, usuario.getEndereco().getEnderecoId());
+            st.setInt(7, usuario.getFuncao().getFuncaoId());
+            
+            st.executeUpdate();
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao inserir o usuário: " + usuario.getNomeCompleto(), e);
+        }
     }
 
     @Override
-    public void atualizar(Usuario t) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void atualizar(Usuario usuario) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(ATUALIZAR)) {
+            // Condicional WHERE para atualizar os dados
+            st.setInt(8, usuario.getUsuarioId());
+            
+            // Dados a serem atualizados
+            st.setString(1, usuario.getNomeCompleto());
+            st.setString(2, usuario.getEmail());
+            st.setString(3, usuario.getCpf());
+            st.setString(4, usuario.getTelefone());
+            st.setString(5, usuario.getSenha());
+            st.setInt(6, usuario.getEndereco().getEnderecoId());
+            st.setInt(7, usuario.getFuncao().getFuncaoId());
+            
+            st.executeUpdate();
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao atualizar o usuário: " + usuario.getNomeCompleto(), e);
+        }
     }
 
     @Override
-    public void remover(Usuario t) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void remover(Usuario usuario) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(DELETAR)) {
+            st.setInt(1, usuario.getUsuarioId());
+            
+            st.executeUpdate();
+        } catch(SQLException e) {
+            throw new DAOException("Erro DAO: Problema ao remover o usuário: " + usuario.getNomeCompleto(), e);
+        }
     }
 
 }
