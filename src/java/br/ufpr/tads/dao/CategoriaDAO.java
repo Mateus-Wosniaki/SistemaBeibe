47564ab784ce8901c9ee095039d00e5e506bd10a
Gabril <gjperes@ufpr.br>
2022-09-07 18:01:17 -0300

    Implementação da CategoriaDAO

--- src/java/br/ufpr/tads/dao/CategoriaDAO.java
+++ src/java/br/ufpr/tads/dao/CategoriaDAO.java
@@ -9,6 +9,8 @@ import br.ufpr.tads.beans.Categoria;
 import br.ufpr.tads.exception.DAOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,19 +18,28 @@ import java.util.List;
  * @author Gabril
  */
 public class CategoriaDAO implements DAO<Categoria> {
-    private static final String INSERIR = "insert into MUDAR (*, *, *) values (?, ?, ?)";
-    private static final String BUSCARTODOS = "select * from MUDAR";
-    private static final String BUSCARPORID = "select * from MUDAR where MUDAR = ?";
-    private static final String DELETAR = "delete from MUDAR where MUDAR = ?";
-    private static final String ATUALIZAR = "update MUDAR set MUDAR = ?, MUDAR = ?, MUDAR = ? where MUDAR = ?";
+    private static final String INSERIR = "insert into public.Categoria (nomeCategoria) values (?)";
+    private static final String BUSCARTODOS = "select idCategoria, nomeCategoria from public.Categoria";
+    private static final String BUSCARPORID = "select idCategoria, nomeCategoria from public.Categoria where idCategoria = ?";
+    private static final String DELETAR = "delete from public.Categoria where idCategoria = ?";
+    private static final String ATUALIZAR = "update public.Categoria set nomeCategoria = ? where idCategoria = ?";
 
     private Connection con = null;
 
     @Override
     public Categoria buscar(int id) throws DAOException {
         try (PreparedStatement st = con.prepareStatement(BUSCARPORID)) {
-            // TODO: implementar
-            return null;
+            Categoria categoria = new Categoria();
+            
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            
+            if (rs.next()) {
+                categoria.setCategoriaId(rs.getInt("idCategoria"));
+                categoria.setDescricao(rs.getString("nomeCategoria"));
+            }
+            
+            return categoria;
         } catch(SQLException e) {
             throw new DAOException("Erro DAO: Problema ao recuperar a categoria ID: " + id, e);
         }
@@ -37,8 +48,19 @@ public class CategoriaDAO implements DAO<Categoria> {
     @Override
     public List<Categoria> buscarTodos() throws DAOException {
         try (PreparedStatement st = con.prepareStatement(BUSCARTODOS)) {
-            // TODO: implementar
-            return null;
+            List<Categoria> categorias = new ArrayList<>();
+            
+            ResultSet rs = st.executeQuery();
+            while(rs.next()) {
+                Categoria categoria = new Categoria();
+                
+                categoria.setCategoriaId(rs.getInt("idCategoria"));
+                categoria.setDescricao(rs.getString("nomeCategoria"));
+                
+                categorias.add(categoria);
+            }
+            
+            return categorias;
         } catch(SQLException e) {
             throw new DAOException("Erro DAO: Problema ao recuperar todas categorias", e);
         }
@@ -46,8 +68,14 @@ public class CategoriaDAO implements DAO<Categoria> {
 
     @Override
     public void inserir(Categoria categoria) throws DAOException {
-        try (PreparedStatement st = con.prepareStatement(INSERIR)) {
-            // TODO: implementar
+        try (PreparedStatement st = con.prepareStatement(INSERIR, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            st.setString(1, categoria.getDescricao());
+            st.executeUpdate();
+            
+            ResultSet rs = st.getGeneratedKeys();
+            if (rs.next()) {
+                categoria.setCategoriaId(rs.getInt(1));
+            }
         } catch(SQLException e) {
             throw new DAOException("Erro DAO: Problema ao inserir a categoria: " + categoria.getDescricao(), e);
         }
@@ -56,7 +84,9 @@ public class CategoriaDAO implements DAO<Categoria> {
     @Override
     public void atualizar(Categoria categoria) throws DAOException {
         try (PreparedStatement st = con.prepareStatement(ATUALIZAR)) {
-            // TODO: implementar
+            st.setString(1, categoria.getDescricao());
+            st.setInt(2, categoria.getCategoriaId());
+            st.executeUpdate();
         } catch(SQLException e) {
             throw new DAOException("Erro DAO: Problema ao atualizar a categoria: " + categoria.getDescricao(), e);
         }
@@ -65,7 +95,8 @@ public class CategoriaDAO implements DAO<Categoria> {
     @Override
     public void remover(Categoria categoria) throws DAOException {
         try (PreparedStatement st = con.prepareStatement(DELETAR)) {
-            // TODO: implementar
+            st.setInt(1, categoria.getCategoriaId());
+            st.executeUpdate();
         } catch(SQLException e) {
             throw new DAOException("Erro DAO: Problema ao remover a categoria: " + categoria.getDescricao(), e);
         }
