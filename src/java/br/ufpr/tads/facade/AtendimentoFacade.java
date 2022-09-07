d23797359d1948a065e2f893f1c2f02d78276053
Gabril <gjperes@ufpr.br>
2022-09-07 19:58:00 -0300

    Finalização do AtendimentoFacade + impl. necessárias na DAO

--- src/java/br/ufpr/tads/facade/AtendimentoFacade.java
+++ src/java/br/ufpr/tads/facade/AtendimentoFacade.java
@@ -5,90 +5,87 @@
 package br.ufpr.tads.facade;
 
 import br.ufpr.tads.beans.Atendimento;
+import br.ufpr.tads.dao.AtendimentoDAO;
+import br.ufpr.tads.dao.ConnectionFactory;
 import br.ufpr.tads.exception.AtendimentoException;
-import java.util.ArrayList;
+import br.ufpr.tads.exception.DAOException;
 import java.util.List;
 
 /**
  *
- * @author Mateus Wosniaki
+ * @author Mateus Wosniaki, Gabriel Jesus Peres
  */
 public class AtendimentoFacade {
 
     public static List<Atendimento> buscarTodosAtendimentosAbertos() throws AtendimentoException {
-        /*
-        try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
+        try {
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
+            
             List<Atendimento> atendimentos = atendimentoDao.buscarTodosAtendimentosAbertos();
             return atendimentos;
-        }
-        }catch(DAOException ex){
+        } catch(DAOException ex){
             throw new AtendimentoException("Erro ao buscar todos os atendimentos abertos", ex);
         }
-         */
-        return new ArrayList<>();
     }
 
     public static List<Atendimento> buscarTodosAtendimentos() throws AtendimentoException {
-        /*
-        try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
-            List<Atendimento> atendimentos = atendimentoDao.buscarTodosAtendimentos();
+        try {
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
+            
+            List<Atendimento> atendimentos = atendimentoDao.buscarTodos();
             return atendimentos;
-        }
-        }catch(DAOException ex){
+        } catch(DAOException ex){
             throw new AtendimentoException("Erro ao buscar todos os atendimentos", ex);
         }
-         */
-        return new ArrayList<>();
     }
 
     public static List<Atendimento> buscarAtendimentosPorCliente(int clienteId) throws AtendimentoException {
-        /*
         try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
             List<Atendimento> atendimentos = atendimentoDao.buscarAtendimentosCliente(clienteId);
             return atendimentos;
-        }
         }catch(DAOException ex){
             throw new AtendimentoException("Erro ao buscar atendimentos por cliente", ex);
         }
-         */
-        return new ArrayList<>();
     }
 
     public static Atendimento buscarAtendimentoPorId(int atendimentoId) throws AtendimentoException {
-        /*
         try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
-            Atendimento atendimento = atendimentoDao.buscarAtendimento(atendimentoId);
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
+            
+            Atendimento atendimento = atendimentoDao.buscar(atendimentoId);
             return atendimento;
         }catch(DAOException ex){
             throw new AtendimentoException("Erro buscando atendimento por id", ex);
         }
-         */
-        return new Atendimento();
     }
 
     public static void criarAtendimento(Atendimento atendimento) throws AtendimentoException {
-        /*
         try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
-            atendimentoDao.criarAtendimento(atendimento);
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
+            
+            atendimentoDao.inserir(atendimento);
         }catch(DAOException ex){
             throw new AtendimentoException("Erro criando atendimento", ex);
         }
-         */
     }
 
     public static void deletarAtendimento(int atendimentoId) throws AtendimentoException {
-        /*
         try{
-            AtendimentoDAO atendimentoDao = new AtendimentoDAO();
-            atendimentoDao.deletarAtendimento(atendimentoId);
+            ConnectionFactory connF = new ConnectionFactory();
+            AtendimentoDAO atendimentoDao = new AtendimentoDAO(connF.getConnection());
+            
+            Atendimento atendimento = new Atendimento();
+            atendimento.setAtendimentoId(atendimentoId);
+            
+            atendimentoDao.remover(atendimento);
         }catch(DAOException ex){
             throw new AtendimentoException("Erro deletando atendimento", ex);
         }
-         */
     }
 }
